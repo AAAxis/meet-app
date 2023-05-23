3d31ece54b22c5bdcedc0870a68c92e98ef3ea30
Dima Polskoy <polskoydm@gmail.com>
2023-05-22 22:12:55 -0600

    check

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -19,9 +19,9 @@
       <br><br> 
       <h1>Welcome to MeetApp</h1>
 
-      <br><br> 
+      <br>
       <a href="https://polskoydm.pythonanywhere.com/static/android-app-wheels.apk" download="">
- <img src="https://images.ctfassets.net/23u853certza/1Djo4jOj0doR5PfWVzj9O6/d52acac7f94db66263f5ad9e01c41c82/google-play-badge.png" width="200px;"></a> <br><br>
+ <img src="https://images.ctfassets.net/23u853certza/1Djo4jOj0doR5PfWVzj9O6/d52acac7f94db66263f5ad9e01c41c82/google-play-badge.png" width="200px;"></a> <br>
  <a href="https://polskoydm.pythonanywhere.com/static/iphone-app-wheels.mobileconfig" download="">
 <img src="https://images.ctfassets.net/23u853certza/7xaqvusYmbDlca5umD9bZo/a0fa3e1c7ca41a70c6285d6c7b18c92b/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg" width="200px;"></a>
 
@@ -80,7 +80,7 @@ export default {
 
 .centered-text {
   position: absolute;
-  top: 60%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   text-align: center;
