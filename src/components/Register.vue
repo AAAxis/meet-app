df8904694ae32a522d8305ed583f493da4c158d1
Dima Polskoy <polskoydm@gmail.com>
2023-05-24 00:45:31 -0600

    client id

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>Registration Form</h1>
-    <button @click="registerWithGoogle">Register with Google</button>
+    <div id="g_id_onload"
+         data-client_id="45791953662-3b6f3cirn7sqm3iif1blfuen8dh2tu48.apps.googleusercontent.com"
+         data-callback="handleCredentialResponse"></div>
     <div v-if="registeredEmail">
       <h2>Registered Email:</h2>
       <p>{{ registeredEmail }}</p>
@@ -13,34 +15,36 @@
 export default {
   data() {
     return {
-      registeredEmail: '',
+      registeredEmail: ''
     };
   },
+  mounted() {
+    this.loadGooglePlatform();
+  },
   methods: {
-    async registerWithGoogle() {
-      try {
-        const auth2 = await gapi.auth2.init({
-          client_id: '45791953662-3b6f3cirn7sqm3iif1blfuen8dh2tu48.apps.googleusercontent.com',
-        });
-        const googleUser = await auth2.signIn();
-        const id_token = googleUser.getAuthResponse().id_token;
-
+    loadGooglePlatform() {
+      const script = document.createElement('script');
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.async = true;
+      script.defer = true;
+      document.head.appendChild(script);
+    },
+    handleCredentialResponse(response) {
+      const credential = response.credential;
+      const email = credential && credential.id;
+      
+      if (email) {
         // Perform client-side registration logic
-        this.registerClientSide(id_token);
-      } catch (error) {
-        console.error('An error occurred:', error);
+        this.registerClientSide(email);
       }
     },
-    registerClientSide(id_token) {
+    registerClientSide(email) {
       // Replace this code with your client-side registration logic
-      // For example, you can save the token to local storage or perform any other client-side operations
+      // For example, you can save the email to local storage or perform any other client-side operations
 
       // After registration, set the registered email
-      const profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
-      this.registeredEmail = profile.getEmail();
-
-      console.log('Token:', id_token);
-    },
-  },
+      this.registeredEmail = email;
+    }
+  }
 };
 </script>
