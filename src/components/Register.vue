65b18581d4c501bcf6809a6705d589ce435184fe
Dima Polskoy <polskoydm@gmail.com>
2023-05-24 00:28:27 -0600

    depracted

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -8,51 +8,55 @@
 <script>
 export default {
   methods: {
-    registerWithGoogle() {
-      gapi.load('auth2', () => {
-        gapi.auth2.init({
+    async registerWithGoogle() {
+      try {
+        const auth2 = await gapi.auth2.init({
           client_id: '45791953662-3b6f3cirn7sqm3iif1blfuen8dh2tu48.apps.googleusercontent.com',
-        }).then(auth2 => {
-          auth2.signIn().then(googleUser => {
-            const id_token = googleUser.getAuthResponse().id_token;
-
-            // Send the id_token to your Flask backend for registration
-            this.sendToken('https://rachinsky.pythonanywhere.com/register', id_token);
-          });
         });
-      });
+        const googleUser = await auth2.signIn();
+        const id_token = googleUser.getAuthResponse().id_token;
+
+        // Send the id_token to your Flask backend for registration
+        await this.sendToken('https://rachinsky.pythonanywhere.com/register', id_token);
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
     },
-    loginWithGoogle() {
-      gapi.load('auth2', () => {
-        gapi.auth2.init({
+    async loginWithGoogle() {
+      try {
+        const auth2 = await gapi.auth2.init({
           client_id: '45791953662-3b6f3cirn7sqm3iif1blfuen8dh2tu48.apps.googleusercontent.com',
-        }).then(auth2 => {
-          auth2.signIn().then(googleUser => {
-            const id_token = googleUser.getAuthResponse().id_token;
-
-            // Send the id_token to your Flask backend for login
-            this.sendToken('https://rachinsky.pythonanywhere.com/login', id_token);
-          });
         });
-      });
+        const googleUser = await auth2.signIn();
+        const id_token = googleUser.getAuthResponse().id_token;
+
+        // Send the id_token to your Flask backend for login
+        await this.sendToken('https://rachinsky.pythonanywhere.com/login', id_token);
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
     },
-    sendToken(url, token) {
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ google_token: token })
-      })
-        .then(response => response.json())
-        .then(data => {
+    async sendToken(url, token) {
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ google_token: token })
+        });
+
+        if (response.ok) {
+          const data = await response.json();
           // Handle the response from the backend
           console.log(data);
-        })
-        .catch(error => {
-          // Handle the error
-          console.error('An error occurred:', error);
-        });
+        } else {
+          throw new Error('Request failed');
+        }
+      } catch (error) {
+        // Handle the error
+        console.error('An error occurred:', error);
+      }
     }
   },
 };
